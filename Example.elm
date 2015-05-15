ef9bff125cee172100fa3fd57431a8469350cebc
Philipp Markovics <philipp@markovics.com>
2015-05-15 18:50:08 +0200

    Add note about using mailboxes.

--- Example.elm
+++ Example.elm
@@ -5,3 +5,7 @@ import Graphics.Element exposing (..)
 main : Element
 main =
   show 1
+
+-- TODO: Try using mailboxes for animation states similar to:
+-- http://elm-lang.org/edit/examples/Intermediate/Flickr.elm
+
